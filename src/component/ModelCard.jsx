0281320c619a5c633f68a38c76141daf506fe926
hossain15-5299-creator <hossain15-5299@diu.edu.bd>
2026-04-01 00:01:35 +0600

    Code Updated 5

--- src/component/ModelCard.jsx
+++ src/component/ModelCard.jsx
@@ -26,12 +26,19 @@ const ModelCard = ({ model, carts, setCarts }) => {
                 </div>
             </div>
 
+                {/*Card- title, description  */}
             <div className="p-4">
-                <h2 className="text=2xl font-bold">{model.title}</h2>
+                <h2 className="text=2xl font-bold mb-2">{model.title}</h2>
                 <p>{model.description}</p>
 
-                {/*  */}
-                <ul className="space-y-2 mb-6 text-sm text-[#475569]">
+                {/* Card-Price/month */}
+            <div className="lex items-baseline gap-1 mt-1 text-black px-4 mb-4">
+                <span className="text-2xl font-bold">${model.price}</span>
+                <span className="text-gray-500 text-sm">{model.price_type}</span>
+            </div>
+
+                {/* Card Speciality lists*/}
+                <ul className="space-y-2 mb-4 mt-4 text-sm text-[#475569]">
                     {model.features.map((feature, index) => (
                         <li key={index} className="flex items-center gap-2">
                             <span className="text-[#10B981] font-bold">✓</span> {feature}
@@ -40,12 +47,8 @@ const ModelCard = ({ model, carts, setCarts }) => {
                 </ul>
             </div>
 
-            <div className="mb-4">
-                <span className="text-2xl font-bold">${model.price}</span>
-                <span className="text-gray-500 text-sm">{model.price_type}</span>
-            </div>
+                    {/* Card Buy now Button */}
             <div className="">
-
                 <button onClick={handleBuying} className={`w-full py-2 rounded font-semibold text-white transition ${isBought ? "bg-[#10B981] cursor-not-allowed" : "bg-[#7C3AED] hover:bg-[#6D28D9]"}`} disabled={isBought}> {isBought ? "Added to cart" : "Buy now"}
 
                 </button>
